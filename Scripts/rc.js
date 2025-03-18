918a61992985a311f9e9d697ab5f5a46826fbfe7
bpxtc <118289528+bpxtc@users.noreply.github.com>
2025-03-18 09:34:22 +0800

    Add files via upload

---
+++ Scripts/rc.js
@@ -0,0 +1,36 @@
+let obj = {};
+
+let creakinfo = {"appId":"com.uzero.cn.FoJiCam","productId":"com.uzero.cn.fojicam.month1","entitlements":["Pro"]}
+
+if(typeof $response == "undefined") {
+  delete $request.headers["x-revenuecat-etag"];
+  delete $request.headers["X-RevenueCat-ETag"];
+  obj.headers = $request.headers;
+}else {
+  let body = JSON.parse(typeof $response != "undefined" && $response.body || null);
+  if(body && body.subscriber) {
+    let date = {"expires_date": "2999-01-01T00:00:00Z","original_purchase_date":"2021-01-01T00:00:00Z","purchase_date": "2021-01-01T00:00:00Z","ownership_type": "PURCHASED","store": "app_store"};
+    let subscriber = body.subscriber;
+    const newObj = Object.fromEntries(Object.entries($request.headers).map(([k, v]) => [k.toLowerCase(), v]));
+    let app_id = newObj["x-client-bundle-id"]?newObj["x-client-bundle-id"]:newObj["user-agent"].match(/^[-_%a-zA-Z0-9]+/)[0];
+    const list = [
+      creakinfo
+   ];  
+   for(let data of list){
+     if(app_id == data.appId){
+       let product_id = data.productId;
+       let entitlements = data.entitlements;
+       subscriber.subscriptions[(product_id)] = date;
+       for (let entitlement of entitlements) {
+         subscriber.entitlements[(entitlement)] = date;        
+         subscriber.entitlements[(entitlement)].product_identifier = product_id; 
+       }
+       break; 
+     }   
+   }
+   obj.body = JSON.stringify(body);
+  }
+}
+
+$done(obj);
+
